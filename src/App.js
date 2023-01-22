6d2cda03cda990ed8a2e13c7bfc44a9cafbc48a5
Jose Soto <jsoto7087@gmail.com>
2023-01-22 06:19:16 -0500

    Added npm big calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import DatePicker from "react-datepicker"
 import './App.css';
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+const localizer = dateFnsLocalizer({
+  format, 
+  parse,
+  startOfWeek,
+  getDay, 
+  locales
+
+})
+
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true, 
+    start: new Date(2023,1,1),
+    end: new Date(2023,2,2)
+  },
+  {
+    title: "Other Big Meeting",
+    allDay: true, 
+    start: new Date(2023,1,6),
+    end: new Date(2023,2,2)
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <Calendar 
+      localizer={localizer} 
+      events={events} 
+      startAccessor="start" 
+      endAccessor="end"
+      style={{height: 500}}
+    />
+
     </div>
   );
 }
